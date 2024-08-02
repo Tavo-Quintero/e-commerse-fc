63800db49803cca4fc7a50b53a4466bcfa652c97
Edward Tapiero <edwardtapiero28@gmail.com>
2024-08-01 23:55:20 -0500

    prueva 7

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL || `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
-  dialectOptions: process.env.DATABASE_URL ? {
+  dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false
     }
-  } : {}
+  }
 });
 
 sequelize.authenticate()

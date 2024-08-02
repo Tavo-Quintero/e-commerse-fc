9180302c8f1877b17ef78ddf96f8763ff462710e
Edward Tapiero <edwardtapiero28@gmail.com>
2024-08-01 23:13:36 -0500

    prueba

--- config/config.js
+++ config/config.js
@@ -1,20 +1,9 @@
-require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
+const sequelize = new Sequelize('sneakers', 'postgres', '123', {
+    host: 'localhost',
+    dialect: 'postgres',
+    logging: false,  
 });
-
-sequelize.authenticate()
-  .then(() => console.log('Connection has been established successfully.'))
-  .catch(err => console.error('Unable to connect to the database:', err));
 
 module.exports = sequelize;
--- db.js
+++ db.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  user: process.env.DB_USER,          
+  host: process.env.DB_HOST,     
+  database: process.env.DB_NAME,     
+  password: process.env.DB_PASSWORD, 
+  port: process.env.DB_PORT, 
+  database: process.env.DATABASE_URL           
 });
 
 pool.connect((err, client, release) => {

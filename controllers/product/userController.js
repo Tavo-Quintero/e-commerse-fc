ab3f323bcb9b9ba92bb5e4170191dcf815cfca18
Edward Tapiero <edwardtapiero28@gmail.com>
2024-08-16 17:29:34 -0500

    agregar ban

--- controllers/product/userController.js
+++ controllers/product/userController.js
@@ -6,10 +6,10 @@ const jwt = require('jsonwebtoken');
 // Registrar un nuevo usuario
 exports.register = async (req, res) => {
     try {
-        const { username, email, password, isAdmin } = req.body;
+        const { username, email, password, isAdmin, ban } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const user = await User.create({ username, email, password: hashedPassword, isAdmin });
+        const user = await User.create({ username, email, password: hashedPassword, isAdmin, ban });
         res.status(201).json(user);
     } catch (error) {
         res.status(500).json({ message: 'Error registering user' });

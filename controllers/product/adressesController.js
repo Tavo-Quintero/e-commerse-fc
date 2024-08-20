79cd5fceea66c7ddb617108773afeb3a81dd0c7e
Edward Tapiero <edwardtapiero28@gmail.com>
2024-08-19 19:59:06 -0500

    encontrar error

--- controllers/product/adressesController.js
+++ controllers/product/adressesController.js
@@ -5,7 +5,7 @@ exports.getAllAddresses = async (req, res) => {
         const addresses = await Addresses.findAll();
         res.json(addresses);
     } catch (error) {
+        console.error('Error fetching getAllAddresses:', error.message, error.stack);
         res.status(500).json({ message: 'Error fetching getAllAddresses' });
     }
 };
-

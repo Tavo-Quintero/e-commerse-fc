febb19230331174b2ea8f391bec89b20a2793c36
Edward Tapiero <edwardtapiero28@gmail.com>
2024-08-23 14:17:58 -0500

    prueba sopcket

--- config/socket/socket.js
+++ config/socket/socket.js
@@ -1,49 +1,51 @@
-const socketIo = require('socket.io');
-const cors = require('cors');
+const { Server } = require('socket.io');
 
 let io;
 
 module.exports = {
     init: (server) => {
-        io = socketIo(server, {
+        io = new Server(server, {
             cors: {
-                origin: "https://e-commerse-fc.onrender.com",
-                methods: ["GET", "POST"]
+                origin: 'https://pf-henry-trabajofinal.vercel.app', // Reemplaza con la URL de tu cliente
+                methods: ["GET", "POST"],
+                credentials: true
             }
         });
 
         io.on('connection', (socket) => {
-            console.log('New client connected');
+            console.log('Usuario conectado');
 
             // Unirse a la sala específica del usuario
             socket.on('joinRoom', (userId) => {
                 socket.join(userId);
-                console.log(`User ${userId} joined room`);
+                console.log(`Usuario ${userId} se unió a la sala`);
             });
 
             // Mensajes del usuario
             socket.on('userMessage', (data) => {
                 const { userId, message } = data;
                 io.to(userId).emit('adminMessage', message); // Enviar mensaje al administrador
-                // Guarda el mensaje en la base de datos si es necesario
+                // Aquí puedes guardar el mensaje en la base de datos si es necesario
             });
 
             // Mensajes del administrador
             socket.on('adminMessage', (data) => {
                 const { userId, message } = data;
                 io.to(userId).emit('userMessage', message); // Enviar mensaje al usuario
-                // Guarda el mensaje en la base de datos si es necesario
+                // Aquí puedes guardar el mensaje en la base de datos si es necesario
             });
 
             socket.on('disconnect', () => {
-                console.log('Client disconnected');
+                console.log('Usuario desconectado');
             });
         });
+
         return io;
     },
+
     getIo: () => {
         if (!io) {
-            throw new Error('Socket.io not initialized!');
+            throw new Error('¡Socket.io no está inicializado!');
         }
         return io;
     }
